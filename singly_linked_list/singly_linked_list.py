c1fac66bc4f1cab2fc3230c332b8116a245a419e
unknown <jjbreig@gmail.com>
2020-09-17 15:03:28 -0400

    corrected remove tail return values, tests now passing

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,85 @@
+class Node: 
+    def __init__(self, value = None, next_node = None):
+        self.value = value
+        self.next_node = next_node
+    
+    def get_value(self):
+        return self.value
+    
+    def get_next_node(self):
+        return self.next_node
+    
+    def set_next_node(self, new_next):
+        self.next_node = new_next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def add_to_head(self, value):
+        new_node = Node(value)
+        if self.head is None: 
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            new_node.set_next_node(self.head)
+            self.head = new_node
+        
+    def add_to_tail(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            self.tail.set_next_node(new_node)
+            self.tail = new_node
+    
+    def remove_head(self):
+        if self.head is None: 
+            return None
+        else:
+            ret_value = self.head.get_value()
+
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            
+            else: 
+                self.head = self.head.get_next_node()
+            return ret_value
+
+    def remove_tail(self):
+        if self.head is None:
+            return None
+        
+        if self.head.get_next_node() == None:
+            ret_value = self.head
+            self.head = None
+            self.tail = None
+            return ret_value.get_value()
+
+        current = self.head
+        prev = current
+        while current.get_next_node() is not None:
+            prev = current
+            current = current.get_next_node()
+        ret_value = current    
+        prev.set_next_node(None)
+        return ret_value.get_value()
+                
+
+            
+
+    def contains(self, value):
+        cur_node = self.head
+        while cur_node is not None:
+            if cur_node.get_value == value:
+                return True
+        return False
+
+    def get_max(self):
+        pass
