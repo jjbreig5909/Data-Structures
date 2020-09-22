f0e6529d5344c710eb29ef077bf51bd95484e69b
Jeff Breig <jjbreig@gmail.com>
2020-09-21 21:41:20 -0400

    all but one test passing

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -10,9 +10,9 @@ class ListNode:
             
     def delete(self):
         if self.prev:
-            self.next.prev = node.prev
+            self.prev.next = self.next
         if self.next:
-            self.prev.next = node.next
+            self.next.prev = self.prev
 """
 Our doubly-linked list class. It holds references to 
 the list's head and tail nodes.
@@ -52,7 +52,19 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
+        if self.head is None: 
+            return None
+        elif self.head == self.tail:
+            ret_value = self.tail.value
+            self.head = None
+            self.tail = None
+            self.length -=1
+            return ret_value
+        else:
+            ret_value = self.head.value
+            self.head = self.head.next
+            self.length -=1
+            return ret_value
             
     """
     Wraps the given value in a ListNode and inserts it 
@@ -60,7 +72,17 @@ class DoublyLinkedList:
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value)
+
+        if self.tail is None: 
+            self.head = new_node
+            self.tail = new_node
+            self.length +=1
+        else: 
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+            self.length +=1
             
     """
     Removes the List's current tail node, making the 
@@ -68,7 +90,20 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
+        if self.tail is None: 
+            return None
+        elif self.head == self.tail:
+            ret_value = self.tail.value
+            self.head = None
+            self.tail = None
+            self.length -=1
+            return ret_value
+        else:
+            ret_value = self.tail.value
+            self.tail = self.tail.prev
+            self.length -=1
+            return ret_value
+
             
     """
     Removes the input node from its current spot in the 
@@ -86,7 +121,10 @@ class DoublyLinkedList:
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return
+        self.delete(node)
+        self.add_to_tail(node.value)
 
     """
     Deletes the input node from the List, preserving the 
@@ -99,18 +137,40 @@ class DoublyLinkedList:
         elif self.head is self.tail:
             self.head = None
             self.tail = None
+            self.length -=1
         elif node is self.head:
             self.head = node.next
             node.delete()
+            self.length -=1
         elif node is self.tail:
             self.tail = node.prev
             node.delete()
+            self.length -=1
         else:
             node.delete()
+            self.length -=1
 
     """
     Finds and returns the maximum value of all the nodes 
     in the List.
     """
     def get_max(self):
-        pass
+        if self.head is None:
+            return None
+        elif self.head == self.tail:
+            return self.head.value
+        else:
+            current_node = self.head
+            max_value = current_node.value
+            while current_node.next is not self.tail:
+                print("This is the max, didn't pass if: ", max_value)
+                if current_node.value > max_value:
+                    max_value = current_node.value
+                    print("This is the max: ", max_value)
+                current_node = current_node.next
+            if self.tail.value>max_value:
+                max_value = self.tail.value
+                return max_value
+                
+            else:
+                return max_value
